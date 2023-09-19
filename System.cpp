2403a66e99a5656fccd5655cbd757a4e6a50b9e1
Paulappaul <Gustavskiss@gmail.com>
2023-09-19 05:17:47 -0700

    GUI CHANGE

--- System.cpp
+++ System.cpp
@@ -144,7 +144,12 @@ struct mainData
 	Fl_Input* newUsername;
     Fl_Input* newPassword;
 	Fl_Widget* previousParent;
+	Fl_Text_Buffer* orderList;
 	std::shared_ptr<std::string> dailymessage;
+	std::vector<std::string> currentOrder;
+	std::vector < std::vector<std::string>>previousOrder;
+	Till* systemTillptr;
+	double orderPrice = 0;
 	
 };
 
@@ -163,7 +168,7 @@ std::vector<User*>& System::getUserlist()
 
 std::vector<Food>& System::getFoodList()
 {
-
+	std::cout << "getting food list" << std::endl;
 	return System::foodList;
 
 }
@@ -179,7 +184,7 @@ void managerControlCallback(Fl_Widget* widget, void* data)
 		md->previousParent = widget;
 		Fl_Window* mainWindow = new Fl_Window(1800, 1000, "Manager Window");
 		Fl_Box* mainBox = new Fl_Box(0, 0, 1800, 1000);
-		Fl_PNG_Image* wallpaper = new Fl_PNG_Image("Wallpaper\\bg.png");
+		Fl_PNG_Image* wallpaper = new Fl_PNG_Image("Wallpaper\\red.png");
 		mainBox->image(wallpaper);
 		const int top = 30;
 		const int rightoff = 30;
@@ -254,7 +259,17 @@ struct inventoryUI
 	std::shared_ptr<std::vector<std::string>> breakfastList;
 	int type = 0; 
 	Fl_Widget* previousParent;
+	//Fl_Text_Buffer* tb;
+	
 
+};
+
+struct dataPass
+{
+	std::string sP;
+	double price;
+	Fl_Text_Buffer* tb;
+	double* totalPrice; 
 
 };
 
@@ -268,14 +283,16 @@ void inventoryCallback(Fl_Widget* widget, void* data)
 	widget->parent()->hide();
 
 	mainData* md = static_cast<mainData*>(data);
-	inventoryUI* iUI = new inventoryUI;
+	inventoryUI* iUI = new inventoryUI; //make sure this is deleted
 
 	iUI->previousParent = widget->parent();
 	std::vector<Food> foodList = md->thisSystem->getFoodList();
 
+	
+
 	Fl_Window* mainWindow = new Fl_Window(1800, 1000, "Inventory");
 	Fl_Box* mainBox = new Fl_Box(0, 0, 1800, 1000);
-	Fl_PNG_Image* wallpaper = new Fl_PNG_Image("Wallpaper\\bg2.png");
+	Fl_PNG_Image* wallpaper = new Fl_PNG_Image("Wallpaper\\blue.png");
 	mainBox->image(wallpaper);
 
 	Fl_Button* backButton = new Fl_Button(100, 800, 150, 100, "@<-");
@@ -312,6 +329,7 @@ void inventoryCallback(Fl_Widget* widget, void* data)
 
 			std::string label = iUI->breakfastList->at(i);
 			const char* clabel = label.c_str();
+			
 
 			iUI->button[buttonIndex] = new Fl_Button(x, y, boxSize, boxSize, strdup(clabel));
 			iUI->button[buttonIndex]->color(FL_DARK_YELLOW);
@@ -368,6 +386,7 @@ void inventoryCallback(Fl_Widget* widget, void* data)
 			{
 				widget->parent()->hide();
 				iUI->previousParent->show();
+				//delete iUI;
 				
 			}
 
@@ -412,7 +431,7 @@ void helpSupportCallback(Fl_Widget* widget, void* data)
 	Fl_Box* mainBox = new Fl_Box(0, 0, 1800, 1000);
 	Fl_Button* backButton = new Fl_Button(100, 700, 200, 200, "@<-");
 
-	Fl_PNG_Image* wallpaper = new Fl_PNG_Image("Wallpaper\\bg4.png");
+	Fl_PNG_Image* wallpaper = new Fl_PNG_Image("Wallpaper\\blue.png");
 	mainBox->image(wallpaper);
 
 	Fl_Button* employeeManual = new Fl_Button(100, 100, 200, 200, "Employee Manual");
@@ -444,7 +463,7 @@ void employeeMessageCallback(Fl_Widget* widget, void* data)
 	Fl_Box* dialogue = new Fl_Box(400, 200, 1000, 600, "TEST");
 	Fl_Button* backButton = new Fl_Button(100, 700, 200, 200, "@<-");
 
-	Fl_PNG_Image* wallpaper = new Fl_PNG_Image("Wallpaper\\bg3.png");
+	Fl_PNG_Image* wallpaper = new Fl_PNG_Image("Wallpaper\\blue.png");
 	mainBox->image(wallpaper);
 
 	
@@ -494,7 +513,7 @@ void reserveTableCallback(Fl_Widget* widget, void* data)
 	Fl_Window* mainWindow = new Fl_Window(1800, 1000, "Reserve Table");
 	Fl_Box* mainBox = new Fl_Box(0, 0, 1800, 1000);
 
-	Fl_PNG_Image* wallpaper = new Fl_PNG_Image("Wallpaper\\bg.png");
+	Fl_PNG_Image* wallpaper = new Fl_PNG_Image("Wallpaper\\red.png");
 	mainBox->image(wallpaper);
 
 	Fl_Button* table1 = new Fl_Button(100, 100, 200, 400, "Table 1");
@@ -527,67 +546,398 @@ void reserveTableCallback(Fl_Widget* widget, void* data)
 	mainWindow->show();
 }
 
-void newOrderCallback(Fl_Widget* widget, void* data) 
-{
-	Fl_Text_Buffer* textBuffer = static_cast<Fl_Text_Buffer*>(data);
 
-	// Clear the existing text in the text display
-	textBuffer->remove(0, textBuffer->length());
+
+void orderCallback(Fl_Widget* widget, void* userData)
+{
+	// Cast the userData back to shared_ptr
+	std::shared_ptr<dataPass>* passPtr = static_cast<std::shared_ptr<dataPass>*>(userData);
+	std::shared_ptr<dataPass> pass = *passPtr;
+	Fl_Text_Buffer* textBuffer = pass->tb;
+
+	std::stringstream s_D;
+	std::string sP = pass->sP;
+	double price = pass->price;
+
+	*(pass->totalPrice) += price;
+	std::cout << "main data total Order Price: " << *(pass->totalPrice);
+
+
+	s_D << price;
+	std::string numStr = s_D.str();
+	std::string result = sP + "\t" + numStr + "\n";
+	const char* resultChar = result.c_str();
+	
+
 
 	// Add the new order information to the text display
-	const char* newOrderText = "New order information goes here.";
-	textBuffer->append(newOrderText);
-
+	const char* newOrderText = pass->sP.c_str();
+	textBuffer->append(resultChar);
 	// Redraw the text display to update its content
 	textBuffer->call_modify_callbacks();
+	
+	
+
 }
 
 
 
+void newOrderCallback(Fl_Widget* widget, void* data) 
+{
+	
+	const int boxSize = 200;
+	const int xOffset = 500;
+	const int yOffset = 100;
+
+	mainData* md = static_cast<mainData*>(data);
+	widget->parent()->hide();
+	//currentOrder is a vecotr that is not dynamically allocated...
+	if (md->currentOrder.size() > 0)
+		md->previousOrder.push_back(md->currentOrder);
+	else
+		std::cout << "no previous orders" << std::endl;
+
+	Fl_Text_Buffer* textBuffer = md->orderList;
+	textBuffer->remove(0, textBuffer->length());
+
+
+	inventoryUI* iUI =		  new inventoryUI; //make sure to delete
+	Fl_Window* mainWindow =	  new Fl_Window(1800, 1000, "New Order");
+	Fl_Box* mainBox =		  new Fl_Box(0, 0, 1800, 1000);
+	Fl_PNG_Image* wallpaper = new Fl_PNG_Image("Wallpaper\\red.png");
+	Fl_Button* backButton =   new Fl_Button(100, 800, 150, 100, "@<-");
+	Fl_Button* nextButton =   new Fl_Button(1550, 800, 150, 100, "@->");
+	
+	std::vector<Food> foodList = md->thisSystem->getFoodList();
+	std::shared_ptr<std::vector<std::string>> breakfastList = std::make_shared<std::vector<std::string>>();
+	std::shared_ptr<std::vector<std::string>> lunchList = std::make_shared<std::vector<std::string>>();
+	std::shared_ptr<std::vector<std::string>> dinnerList = std::make_shared<std::vector<std::string>>();
+	iUI->dinnerList = dinnerList;
+	iUI->lunchList = lunchList;
+	iUI->breakfastList = breakfastList;
+	mainBox->image(wallpaper);
+	iUI->previousParent = widget->parent();
+	for (int i = 0; i < foodList.size(); i++)
+	{
+		if (foodList.at(i).getFoodType() == breakfast)
+			breakfastList->push_back(foodList.at(i).getName());
+		else if (foodList.at(i).getFoodType() == lunch)
+			lunchList->push_back(foodList.at(i).getName());
+		else if (foodList.at(i).getFoodType() == dinner)
+			dinnerList->push_back(foodList.at(i).getName());
+	}
+	
+
+	//Breakfast list
+	int i = 0;
+	for (int row = 0; row < 4; row++)
+	{
+		for (int col = 0; col < 4; col++)
+		{
+			//review use of int and const here
+			const int buttonIndex = row * 4 + col;
+			const int x = col * boxSize + xOffset;
+			const int y = row * boxSize + yOffset;
+
+			std::string label = iUI->breakfastList->at(i);
+			std::string* label2 = new std::string(iUI->breakfastList->at(i));
+			const char* clabel = label.c_str(); // delete? 
+
+			// Create a shared_ptr for dataPass
+			std::shared_ptr<dataPass> pass = std::make_shared<dataPass>();
+			pass->sP = iUI->breakfastList->at(i);
+			pass->price = foodList.at(buttonIndex).getPrice();
+			pass->tb = md->orderList;
+			pass->totalPrice = &(md->orderPrice);
+
+
+			iUI->button[buttonIndex] = new Fl_Button(x, y, boxSize, boxSize, strdup(clabel));
+			iUI->button[buttonIndex]->color(FL_DARK_YELLOW);
+			iUI->button[buttonIndex]->callback(orderCallback, new std::shared_ptr<dataPass>(pass));
+			i++;
+			
+		}
+	}
+
+	;
+	nextButton->callback([](Fl_Widget* widget, void* data)
+		{
+			inventoryUI* iUI = static_cast<inventoryUI*>(data);
+			std::cout << iUI->type << std::endl;
+			if (iUI->type < 2) {
+				iUI->type++;
+
+				for (int row = 0; row < 4; row++) {
+					for (int col = 0; col < 4; col++) {
+						int buttonIndex = row * 4 + col;
+
+						std::string label = "";
+						if (iUI->type == 2)
+						{
+							label = iUI->dinnerList->at(buttonIndex);
+							iUI->button[buttonIndex]->color(FL_RED);
+						}
+						else if (iUI->type == 1)
+						{
+							label = iUI->lunchList->at(buttonIndex);
+							iUI->button[buttonIndex]->color(FL_GREEN);
+						}
+
+						const char* clabel = label.c_str();
+
+						// Free the old label before setting the new one
+						free((void*)iUI->button[buttonIndex]->label());
+
+						iUI->button[buttonIndex]->label(strdup(clabel));
+						iUI->button[buttonIndex]->redraw_label();
+
+					}
+				}
+			}
+		}, iUI);
+
+	std::cout << "passed" << std::endl;
+	backButton->callback([](Fl_Widget* widget, void* data)
+		{
+			inventoryUI* iUI = static_cast<inventoryUI*>(data);
+	std::cout << iUI->type << std::endl;
+	if (iUI->type > -1) {
+		iUI->type--;
+
+		if (iUI->type == -1)
+		{
+			widget->parent()->hide();
+			iUI->previousParent->show();
+		
+
+		}
+
+		for (int row = 0; row < 4; row++) {
+			for (int col = 0; col < 4; col++) {
+				int buttonIndex = row * 4 + col;
+
+				std::string label = "";  
+				if (iUI->type == 1) {
+					label = iUI->lunchList->at(buttonIndex);
+					iUI->button[buttonIndex]->color(FL_GREEN);
+				}
+				else if (iUI->type == 0) {
+					label = iUI->breakfastList->at(buttonIndex);
+					iUI->button[buttonIndex]->color(FL_DARK_YELLOW);
+				}
+
+
+				const char* clabel = label.c_str();
+
+				// Free the old label before setting the new one
+				free((void*)iUI->button[buttonIndex]->label());
+
+				iUI->button[buttonIndex]->label(strdup(clabel));
+				iUI->button[buttonIndex]->redraw_label();
+			}
+		}
+	}
+		}, iUI);
+
+
+	mainWindow->show();
+}
+
+void currentOrderCallback(Fl_Widget* widget, void* data)
+{
+	
+	mainData* md = static_cast<mainData*>(data);
+	md->orderList->remove(0, md->orderList->length());
+	for (std::string line : md->currentOrder)
+	{
+		const char* cLine = line.c_str();
+		md->orderList->append(cLine);
+		md->orderList->append("\n");
+		std::cout << "cLine: " << cLine << std::endl;
+
+	}
+
+}
+
+
+void previousCallback(Fl_Widget* widget, void* data)
+{
+	mainData* md = static_cast<mainData*>(data);
+	
+	md->orderList->remove(0, md->orderList->length());
+	//iterate through each Vector held in the previous order vector
+	for (const std::vector<std::string>& innerVector : md->previousOrder)
+	{
+		//for each vector we dump its conents
+		for (const std::string& str : innerVector) 
+		{
+
+			md->orderList->append(str.c_str());
+			md->orderList->append("\n"); // Add a newline after each string
+		}
+	}
+}
+
+void submitButtonCallback(Fl_Widget* widget, void* data)
+{
+	//repeat code ere...
+	mainData* md = static_cast<mainData*>(data);
+
+	std::cout << "total order price: " << md->orderPrice << "total balance" << md->systemTillptr->getBalance() << std::endl;
+
+	if (md->orderPrice > md->systemTillptr->getBalance())
+	{
+
+		std::cout << "PRICE EXCEEDS TILL BALANCE" << std::endl;
+	}
+	else
+	{
+
+		if (md->currentOrder.size() == 0)
+		{
+			std::cout << "no current order" << std::endl;
+
+			// Get the contents of the text buffer and store in the vector
+			const char* bufferText = md->orderList->text();
+			if (bufferText)
+			{
+				std::string text(bufferText);
+				size_t pos = 0;
+				while ((pos = text.find("\n")) != std::string::npos)
+				{
+					md->currentOrder.push_back(text.substr(0, pos));
+					text.erase(0, pos + 1);
+				}
+				md->currentOrder.push_back(text);  // Add the last line
+			}
+
+			
+			std::string submitedPrice = std::to_string(md->orderPrice);
+			std::string finalPriceMessage = "Checkout: " + submitedPrice;
+			const char* charPass = finalPriceMessage.c_str();
+
+			md->orderList->append(charPass);
+
+			// Print the lines in the vector
+			for (const std::string& line : md->currentOrder)
+			{
+				std::cout << line << std::endl;
+			}
+
+		}
+		else
+		{
+			md->previousOrder.push_back(md->currentOrder);
+			md->currentOrder.clear();
+			std::cout << "current order vector cleared" << std::endl;
+			std::cout << "size of the buffer: " << md->currentOrder.size();
+
+
+			// Get the contents of the text buffer and store in the vector
+			const char* bufferText = md->orderList->text();
+			if (bufferText)
+			{
+				std::string text(bufferText);
+				size_t pos = 0;
+				while ((pos = text.find("\n")) != std::string::npos) {
+					md->currentOrder.push_back(text.substr(0, pos));
+					text.erase(0, pos + 1);
+				}
+				md->currentOrder.push_back(text);  // Add the last line
+			}
+			std::string submitedPrice = std::to_string(md->orderPrice);
+			std::string finalPriceMessage = "Checkout: " + submitedPrice;
+			const char* charPass = finalPriceMessage.c_str();
+
+			md->orderList->append(charPass);
+
+			// Print the lines in the vector
+			for (const std::string& line : md->currentOrder)
+			{
+				std::cout << line << std::endl;
+			}
+
+
+		}
+	}
+
+
+}
+void deleteButtonCallback(Fl_Widget* widget, void* data)
+{
+	//flushes the contents of the current buffer
+	mainData* md = static_cast<mainData*>(data);
+	md->orderList->remove(0, md->orderList->length());
+
+}
+
 void System::posMain(User* currentUser)
 {
+	//GUI Button Parameters
 	const int top = 50;
 	const int rightoff = 75;
 	const int boxSize = 300;
+
+	//data structure intialization 
 	mainData md;
+	Till systemTill; 
+
+	//Assign values to data structures 
 	md.thisSystem = this;
-	md.thisUser = currentUser; // Pass the user pointer
+	md.thisUser = currentUser;			
+	md.systemTillptr = &systemTill;
 
-	createFoodItems(this->getFoodList());
+	//debug
 	md.dailymessage = dailyMesssageDemo();
+	systemTill.setBalance(25.00);
+	createFoodItems(this->getFoodList());
 
-	// Now, you can access the user using md.thisUser as a pointer
-	if (md.thisUser != nullptr)
-	{
-		std::cout << "Logged-in Username: " << md.thisUser->getUsername() << std::endl;
-	}
+	//Username Cast
+	std::string User = "Current User: " + md.thisUser->getUsername();
+	const char* user = User.c_str();
 
-	Fl_Window* mainWindow =        new Fl_Window(1800, 1000, "POS_SYSTEM");
-	Fl_Box* mainBox =			   new Fl_Box(0, 0, 1800, 1000);
-	
-	Fl_PNG_Image* wallpaper =	   new Fl_PNG_Image("Wallpaper\\bg1.png");
-	mainBox->image(wallpaper);
-	
-	//Row 1
-	Fl_Button* newOrder =          new Fl_Button(rightoff,                 top, boxSize, boxSize, "New Order");
-	Fl_Button* outStandingOrders = new Fl_Button(rightoff + boxSize, top, boxSize, boxSize, "Current Orders");
-	Fl_Button* previousOrders =    new Fl_Button(rightoff + (boxSize * 2), top, boxSize, boxSize, "Previous Orders");
+	//Visual Intialization begin
+	Fl_Window* mainWindow =            new Fl_Window(1800, 1000, "POS_SYSTEM");
+	Fl_Box* mainBox =			       new Fl_Box(0, 0, 1800, 1000);
+								      
+	Fl_PNG_Image* wallpaper =	       new Fl_PNG_Image("Wallpaper\\blue.png");
+	mainBox->image(wallpaper);	      
+								      
+	//Row 1						      
+	Fl_Button* newOrder =              new Fl_Button(rightoff,                 top, boxSize, boxSize, "New Order");
+	Fl_Button* outStandingOrders =     new Fl_Button(rightoff + boxSize, top, boxSize, boxSize, "Current Orders");
+	Fl_Button* previousOrders =        new Fl_Button(rightoff + (boxSize * 2), top, boxSize, boxSize, "Previous Orders");
+								      
+	//Row 2						      
+	Fl_Button* reserveTable =          new Fl_Button(rightoff,                 top + boxSize, boxSize, boxSize, "Reserve Table");
+	Fl_Button* inventory =             new Fl_Button(rightoff + boxSize, top + boxSize, boxSize, boxSize, "Inventory");
+	Fl_Button* managerControl =        new Fl_Button(rightoff + (boxSize * 2), top + boxSize, boxSize, boxSize, "Manager Control");
+								      
+	//Row 3						      
+	Fl_Button* helpSupport =           new Fl_Button(rightoff,                 top + boxSize * 2, boxSize, boxSize, "Help/Support");
+	Fl_Button* employeeMessage =       new Fl_Button(rightoff + boxSize, top + boxSize * 2, boxSize, boxSize, "Daily Message");
+	Fl_Button* logout =                new Fl_Button(rightoff + (boxSize * 2), top + boxSize * 2, boxSize, boxSize, "Logout");
 
-	//Row 2
-	Fl_Button* reserveTable =      new Fl_Button(rightoff,                 top + boxSize, boxSize, boxSize, "Reserve Table");
-	Fl_Button* inventory =         new Fl_Button(rightoff + boxSize, top + boxSize, boxSize, boxSize, "Inventory");
-	Fl_Button* managerControl =    new Fl_Button(rightoff + (boxSize * 2), top + boxSize, boxSize, boxSize, "Manager Control");
-
-	//Row 3
-	Fl_Button* helpSupport =       new Fl_Button(rightoff,                 top + boxSize * 2, boxSize, boxSize, "Help/Support");
-	Fl_Button* employeeMessage =   new Fl_Button(rightoff + boxSize, top + boxSize * 2, boxSize, boxSize, "Daily Message");
-	Fl_Button* logout =            new Fl_Button(rightoff + (boxSize * 2), top + boxSize * 2, boxSize, boxSize, "Logout");
-
+	//Order Display
 	Fl_Text_Display* terminalDisplay = new Fl_Text_Display(1150, top, 580, 500);
+	Fl_Text_Buffer* textBuffer =	   new Fl_Text_Buffer();
+
+	//Order Submission
+	Fl_Button* submitButton =		   new Fl_Button(1150, 550, 290, 150, "Submit");
+	Fl_Button* deleteButton =		   new Fl_Button(1440, 550, 290, 150, "Delete");
+
+	//User Label
+	Fl_Box* userLabel =				   new Fl_Box(1300, 900, 300, 100, user);
+
+	//Clock
+	Fl_Clock* clock =				   new Fl_Clock (1350, 750, 200, 150, "Current Time");
+
+	//GUI modifiers
 	terminalDisplay->textfont(FL_COURIER);
 	terminalDisplay->textsize(16);
-	Fl_Text_Buffer* textBuffer = new Fl_Text_Buffer();
+	
 	terminalDisplay->buffer(textBuffer);
+	md.orderList = textBuffer;
 
 	newOrder->color(FL_DARK_RED);
 	outStandingOrders->color(FL_DARK_RED);
@@ -626,32 +976,25 @@ void System::posMain(User* currentUser)
 	managerControl->labelsize(36);
 	logout->labelsize(36);
 
-
+	userLabel->type(FL_UP_BOX);
+	userLabel->labeltype(_FL_SHADOW_LABEL);
+	userLabel->color(FL_WHITE);
 
 	//callbacks
-	newOrder->callback(newOrderCallback, textBuffer);
-	outStandingOrders->callback();
-	previousOrders->callback();
+	newOrder->callback(newOrderCallback, &md);
+	outStandingOrders->callback(currentOrderCallback, &md);
+	previousOrders->callback(previousCallback, &md);
 	reserveTable->callback(reserveTableCallback, &md);
 	inventory->callback(inventoryCallback, &md);
-
-
 	managerControl->callback(managerControlCallback, &md);
-
 	helpSupport->callback(helpSupportCallback, &md);
 	employeeMessage->callback(employeeMessageCallback, &md);
 	logout->callback(logoutCallback, &md);
-
-
-
-	Fl_Clock clock(1300, 600, 300, 300, "Clock");
-
-	Fl_Box* userLabel = new Fl_Box(1300, 800, 300, 100, strdup(md.thisUser->getUsername().c_str()));
-	std::cout << md.thisUser->getUsername() << std::endl;
+	submitButton->callback(submitButtonCallback, &md);
+	deleteButton->callback(deleteButtonCallback, &md);
 
 	mainWindow->show();
 	Fl::run();
-
 	
 }
 
